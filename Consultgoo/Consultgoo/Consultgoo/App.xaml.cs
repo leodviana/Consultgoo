552a72a1506935630f5c53d9d68d80afcc1205d9
Leonardo viana <damasceno.leonardo@gmail.com>
2020-05-04 21:26:12 -0300

    configuracao inicial

---
+++ Consultgoo/Consultgoo/Consultgoo/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Consultgoo
+{
+    public partial class App : PrismApplication
+    {
+        public App() : base(null) { }
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            var mainPage = $"{nameof(NavigationPage)}/{nameof(MainPage)}";
+            await NavigationService.NavigateAsync(mainPage);
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
